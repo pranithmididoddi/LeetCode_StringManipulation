1b36a03f0846e0acd967bbfd50b007a3a098ee2c
pranithmididoddi <pranith13@gmail.com>
2016-12-28 19:47:57 -0600

    first program: Valid Palindrome(LeetCode)

---
+++ src/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Pranith on 12/28/16.
+ */
+public class Solution {
+    public int isPalindrome(String a) {
+        if(isaPalindrome(a)) return 1;
+
+        return 0;
+    }
+
+    public boolean isaPalindrome(String string){
+
+        if(string==null || string.length()==0) return false;
+
+        string=string.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+
+        for(int i=0;i<string.length();i++){
+            if(string.charAt(i)!=string.charAt(string.length()-1-i))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Solution sc=new Solution();
+        String str="A man, a plan, a canal: Panama";
+
+        System.out.println(sc.isPalindrome(str));
+    }
+}
